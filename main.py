8c4ad6538913ac974f3d7da67418f83bfa5a5ff2
Sutsura <sutsura0123@gmail.com>
2024-11-04 16:35:35 +0300

    Создал класс Categories

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sys
 import sqlite3
-from mainWindow_ui import Ui_MainWindow
-from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
+from mainWindow_ui import Ui_MainWindow as mainWindowUi
+from categoriesWindow_ui import Ui_MainWindow as categoriesWindowUi
+from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QMenu
 
 
-class MyWidget(QMainWindow, Ui_MainWindow):
+class MyWidget(QMainWindow, mainWindowUi):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.setFixedSize(575, 688)
         self.con = sqlite3.connect('to_do_list.sqlite')
         cur = self.con.cursor()
 
@@ -19,6 +21,9 @@ class MyWidget(QMainWindow, Ui_MainWindow):
 
         self.tab_changed(0)
         self.tabWidget.currentChanged.connect(self.tab_changed)
+
+        self.editCategoryButtonInTasks.clicked.connect(self.edit_categories)
+        self.editCategoryButtonInEvents.clicked.connect(self.edit_categories)
 
     def tab_changed(self, index):
         self.current_tab = index
@@ -39,6 +44,7 @@ class MyWidget(QMainWindow, Ui_MainWindow):
             else:
                 result = cur.execute(f'''{query} WHERE categories.id = {cur_category}''').fetchall()
             
+            self.update_tasks(result)
         else:
             cur_category = self.categoriesInEvents.currentIndex()
 
@@ -50,8 +56,84 @@ class MyWidget(QMainWindow, Ui_MainWindow):
                 result = cur.execute(query).fetchall()
             else:
                 result = cur.execute(f'''{query} WHERE categories.id = {cur_category}''').fetchall()
+
+            self.update_events(result)
+
+    def update_tasks(self, data):
+        self.tasksTable.setRowCount(len(data))
+        self.tasksTable.setColumnCount(len(data[0]))
+        self.tasksTable.setHorizontalHeaderLabels(['ИД', 'Задача', 'Дата', 'Путь к картинке', 'Категория'])
+
+        for i, row in enumerate(data):
+            for j, col in enumerate(row):
+                self.tasksTable.setItem(i, j, QTableWidgetItem(str(col)))
+
+        self.tasksTable.resizeColumnsToContents()
+
+    def update_events(self, data):
+        self.eventsTable.setRowCount(len(data))
+        self.eventsTable.setColumnCount(len(data[0]))
+        self.eventsTable.setHorizontalHeaderLabels(['ИД', 'Событие', 'Дата', 'Время', 'Категория'])
+
+        for i, row in enumerate(data):
+            for j, col in enumerate(row):
+                self.eventsTable.setItem(i, j, QTableWidgetItem(str(col)))
+
+        self.eventsTable.resizeColumnsToContents()
+
+    def edit_categories(self):
+        self.edit_categories_widget = Categories(parent=self)
+        self.edit_categories_widget.show()
+
+    def add_task(self):
+        pass
+
+    def edit_task(self):
+        pass
+
+    def delete_task(self):
+        pass
+
+    def add_event(self):
+        pass
+
+    def edit_event(self):
+        pass
+
+    def delete_event(self):
+        pass
+
+
+class Categories(QMainWindow, categoriesWindowUi):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.setFixedSize(494, 359)
+        self.update_result()
+
+    def update_result(self):
+        cur = self.parent().con.cursor()
+        query = 'SELECT * FROM categories'
+        result = cur.execute(query).fetchall()
+
+        self.categoriesTable.setColumnCount(len(result[0]))
+        self.categoriesTable.setRowCount(len(result))
+        self.categoriesTable.setHorizontalHeaderLabels(['ИД', 'Категория'])
+
+        for i, row in enumerate(result):
+            for j, col in enumerate(row):
+                self.categoriesTable.setItem(i, j, QTableWidgetItem(str(col)))
         
-        print(result)
+        self.categoriesTable.resizeColumnsToContents()
+
+    def add_category(self):
+        pass
+
+    def edit_category(self):
+        pass
+
+    def edit_category(self):
+        pass
 
 
 def main():
