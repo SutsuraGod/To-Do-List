a88d10be6ac499c5b3f0260fca1e87a87b49b11c
Sutsura <sutsura0123@gmail.com>
2024-11-09 10:38:38 +0300

    начал работу над категориями

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import sys
 import sqlite3
-import re
 
 from mainWindow_ui import Ui_MainWindow as mainWindowUi
 from categoriesWindow_ui import Ui_MainWindow as categoriesWindowUi
 from taskWidget_ui import Ui_Form as taskWidgetUi
 from editTask_ui import Ui_MainWindow as editTaskUi
+from editCategory_ui import Ui_MainWindow as editCategoryUi
 
 from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QMenu, QWidget, QSplitter, QVBoxLayout
 from PyQt6.QtWidgets import QFileDialog, QMessageBox
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QDate
 from PyQt6.QtGui import QPixmap, QImage
 
 
@@ -89,16 +89,26 @@ class MyWidget(QMainWindow, mainWindowUi):
             if widget is not None:
                 self.container_layout.removeWidget(widget)
 
-    def update_events(self, data):
-        self.eventsTable.setRowCount(len(data))
-        self.eventsTable.setColumnCount(len(data[0]))
-        self.eventsTable.setHorizontalHeaderLabels(['ИД', 'Событие', 'Дата', 'Время', 'Категория'])
+    def update_events(self, result=None):
+        if result is None:
+            cur = self.con.cursor()
+            query = '''SELECT * FROM events'''
+            result = cur.execute(query).fetchall()
+        if result:
+            self.eventsTable.setRowCount(len(result))
+            self.eventsTable.setHorizontalHeaderLabels(['Событие', 'Дата', 'Время', 'Категория'])
+            self.eventsTable.setColumnCount(len(result[0][1:]))
 
-        for i, row in enumerate(data):
-            for j, col in enumerate(row):
-                self.eventsTable.setItem(i, j, QTableWidgetItem(str(col)))
+            for i, row in enumerate(result):
+                for j, col in enumerate(row[1:]):
+                    self.eventsTable.setItem(i, j, QTableWidgetItem(str(col)))
 
-        self.eventsTable.resizeColumnsToContents()
+            self.eventsTable.resizeColumnsToContents()
+        else:
+            self.eventsTable.clear()
+            self.eventsTable.setRowCount(0)
+            self.eventsTable.setHorizontalHeaderLabels(['Событие', 'Дата', 'Время', 'Категория'])
+            self.eventsTable.setColumnCount(0)
 
     def edit_categories(self):
         self.edit_categories_widget = Categories(parent=self)
@@ -119,6 +129,8 @@ class MyWidget(QMainWindow, mainWindowUi):
 
 
     def update_combobox(self):
+        self.categoriesInTasks.clear()
+        self.categoriesInEvents.clear()
         cur = self.con.cursor()
         self.categoriesInTasks.addItems(['Все'] + [i[0] for i in cur.execute('SELECT name FROM categories').fetchall()])
         self.filterTasksButton.clicked.connect(self.to_filter)
@@ -139,25 +151,119 @@ class Categories(QMainWindow, categoriesWindowUi):
         query = 'SELECT * FROM categories'
         result = cur.execute(query).fetchall()
 
-        self.categoriesTable.setColumnCount(len(result[0]))
+        self.categoriesTable.setColumnCount(1)
         self.categoriesTable.setRowCount(len(result))
-        self.categoriesTable.setHorizontalHeaderLabels(['ИД', 'Категория'])
+        self.categoriesTable.setHorizontalHeaderLabels(['Категория'])
 
         for i, row in enumerate(result):
-            for j, col in enumerate(row):
-                self.categoriesTable.setItem(i, j, QTableWidgetItem(str(col)))
+            self.categoriesTable.setItem(i, 0, QTableWidgetItem(str(row[1])))
         
         self.categoriesTable.resizeColumnsToContents()
+        self.parent().update_combobox()
 
     def add_category(self):
-        pass
+        self.add_category_widget = editCategory(self)
+        self.add_category_widget.show()
 
     def edit_category(self):
-        pass
+        rows = list(set([i.row() for i in self.categoriesTable.selectedItems()]))
+        if rows and len(rows) == 1:
+            self.statusBar().clearMessage()
+            data = [self.categoriesTable.item(i, 0).text() for i in rows]
+            self.edit_category_widget = editCategory(self, data)
+            self.edit_category_widget.show()
+        elif len(rows) > 1:
+            self.statusBar().showMessage('Нужно выбрать только один элемент')
+        else:
+            self.statusBar().showMessage('Ничего не выбрано')
 
-    def edit_category(self):
-        pass
+    def delete_category(self):
+        rows = list(set([i.row() for i in self.categoriesTable.selectedItems()]))
+        if rows and len(rows) == 1:
+            self.statusBar().clearMessage()
+            data = [self.categoriesTable.item(i, 0).text() for i in rows]
+            valid = QMessageBox.question(
+                self, '', f"Действительно удалить категорию {data[0]}",
+                buttons=QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+        
+            if valid == QMessageBox.StandardButton.Yes:
+                with sqlite3.connect('to_do_list.sqlite') as con:
+                    cur = con.cursor()
+                    cur.execute(f"DELETE FROM categories WHERE name = '{data[0]}'")
+                    con.commit()
+                    self.update_result()
+                    self.parent().update_combobox()
+        elif len(rows) > 1:
+            self.statusBar().showMessage('Нужно выбрать только один элемент')
+        else:
+            self.statusBar().showMessage('Ничего не выбрано')
 
+    def contextMenuEvent(self, event):
+        if self.categoriesTable.geometry().contains(self.mapFromGlobal(event.globalPos())):
+            context_menu = QMenu(self)
+
+            addAction = context_menu.addAction("Добавить")
+            editAction = context_menu.addAction("Изменить")
+            deleteAction = context_menu.addAction("Удалить")
+
+            addAction.triggered.connect(self.add_category)
+            editAction.triggered.connect(self.edit_category)
+            deleteAction.triggered.connect(self.delete_category)
+
+            context_menu.exec(event.globalPos())
+
+
+class editCategory(QMainWindow, editCategoryUi):
+    def __init__(self, parent=None, data=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.setFixedSize(264, 138)
+
+        if not data is None:
+            self.pushButton.setText('Изменить')
+            self.pushButton.clicked.connect(self.edit_elem)
+            self.data = data
+            self.get_elem()
+        else:
+            self.pushButton.setText('Добавить')
+            self.pushButton.clicked.connect(self.add_elem)
+
+    def add_elem(self):
+        category = self.CategoryEdit.toPlainText()
+        
+        if self.get_adding_verdict(category):
+            with sqlite3.connect('to_do_list.sqlite') as con:
+                cur = con.cursor()
+
+                query = f'''INSERT INTO categories(name)
+                            VALUES("{category}")'''
+                cur.execute(query)
+                con.commit()
+                self.close()
+                self.parent().update_result()
+        else:
+            self.statusBar().showMessage('Неверно заполнена форма')
+
+    def edit_elem(self):
+        category = self.CategoryEdit.toPlainText()
+
+        if self.get_editing_verdict(category):
+            pass
+        else:
+            self.statusBar().showMessage('Неверно заполнена форма')
+
+    def get_adding_verdict(self, category):
+        if category.strip():
+            return True
+        return False
+
+    def get_editing_verdict(self, category):
+        if category.strip():
+            return True
+        return False
+
+    def get_elem(self):
+        self.CategoryEdit.setPlainText(self.data[0])
 
 class TaskForm(QWidget, taskWidgetUi):
     def __init__(self, text, data, parent=None):
@@ -233,7 +339,7 @@ class TaskWidget(QMainWindow, editTaskUi):
 
     def add_elem(self):
         name = self.name.toPlainText()
-        date = self.date.text()
+        date = self.date.date().toString('dd.MM.yyyy')
         category = self.category.currentIndex()
 
         if self.get_adding_verdict():
@@ -254,7 +360,7 @@ class TaskWidget(QMainWindow, editTaskUi):
 
     def edit_elem(self):
         name = self.name.toPlainText()
-        date = self.date.text()
+        date = self.date.date().toString('dd.MM.yyyy')
         category = self.category.currentIndex()
 
         if self.get_adding_verdict():
@@ -277,7 +383,7 @@ class TaskWidget(QMainWindow, editTaskUi):
 
     def delete_elem(self):
         name = self.name.toPlainText()
-        date = self.date.text()
+        date = self.date.date().toString('dd.MM.yyyy')
 
         valid = QMessageBox.question(
             self, '', f"Действительно удалить задачу {name}, {date}",
@@ -293,44 +399,16 @@ class TaskWidget(QMainWindow, editTaskUi):
 
     def get_adding_verdict(self):
         name = self.name.toPlainText()
-        date = self.date.text()
-        day_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
         if name.strip() == '':
             return False
-        
-        if date.strip() == '':
-            return False
-        
-        if not re.fullmatch(r'\d{2}\.\d{2}\.\d{4}', date):
-            return False
 
-        if int(date.split('.')[1]) > 12:
-            return False
-
-        if int(date.split('.')[0]) > day_in_month[int(date.split('.')[1]) - 1]:
-            return False
-        
         return True
 
     def get_editing_verdict(self):
         name = self.name.toPlainText()
-        date = self.date.text()
-        day_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
         if name.strip() == '':
-            return False
-        
-        if date.strip() == '':
-            return False
-        
-        if not re.fullmatch(r'\d{2}\.\d{2}\.\d{4}', date):
-            return False
-
-        if int(date.split('.')[1]) > 12:
-            return False
-
-        if int(date.split('.')[0]) > day_in_month[int(date.split('.')[1]) - 1]:
             return False
         
         return True
@@ -355,8 +433,8 @@ class TaskWidget(QMainWindow, editTaskUi):
             self.set_image(self.way_to_picture)
 
     def get_elem(self):
-        self.name.setPlainText(self.data[1]) 
-        self.date.setText(self.data[2])
+        self.name.setPlainText(self.data[1])
+        self.date.setDate(QDate.fromString(self.data[2], 'dd.MM.yyyy'))
         self.category.setCurrentIndex(int(self.data[4]) - 1)
 
 
